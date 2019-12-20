dd71765278fe37605d010be4a1d6201459ae0e54
José Claudio Pereira <jose.claudio@gsurfnet.net>
2019-12-20 14:12:21 -0300

    Primeiro Commit

---
+++ libTxtScreen.c
@@ -0,0 +1,137 @@
+#include "libTxtScreen.h"
+
+#include <math.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <stdarg.h>
+#include <pthread.h>
+
+void scrClear(){
+    printf("\033[2J");
+}
+
+void scrRstCor(){
+    printf("%s", ALL_NORMAL);
+}
+
+void scrSetCor(char *cor){
+    printf("%s", cor);
+}
+
+void scrPrint(char *fgColor, char *bgColor, char *format, ...){
+
+    va_list v1;
+    char buffer[1024];
+
+    va_start(v1, format);
+    vsnprintf(buffer, sizeof(buffer), format, v1);
+    va_end(v1);
+
+    if(fgColor != NULL)
+        scrSetCor(fgColor);
+
+    if(bgColor != NULL)
+        scrSetCor(bgColor);
+    
+    printf("%s", buffer);
+
+    if(fgColor != NULL || bgColor != NULL)
+        scrRstCor();
+}
+
+void scrPrintLn(char *fgColor, char *bgColor, char *format, ...){
+
+    va_list v1;
+    char buffer[1024];
+
+    va_start(v1, format);
+    vsnprintf(buffer, sizeof(buffer), format, v1);
+    scrPrint(fgColor, bgColor, buffer);
+    va_end(v1);
+
+    printf("\n");
+}
+
+void *threadLoad(void *param){
+    TH_LOAD_ARGS *thArgs;
+    thArgs = (TH_LOAD_ARGS *) param;
+    thArgs->cb_scrload(thArgs);
+}
+
+int scrLoad(char *titulo, CB_SCRLOAD cbScrLoad){
+
+    int i, ret;
+    char auxiliar[150];
+
+    char cabecalho[25];
+    int tam_cabecalho = 14;
+    int atual_cabecalho = 0;
+    char buffer_cabecalho[tam_cabecalho+1];
+
+    pthread_t thread;
+    TH_LOAD_ARGS thArgs;
+
+    char tam_barra = 80;
+    char buffer_barra[tam_barra+1];
+
+    int contador = 0;
+    char txtLoading[150];
+
+    memset(cabecalho, 0, sizeof(cabecalho));
+    memset(&thArgs, 0, sizeof(TH_LOAD_ARGS));
+    snprintf(cabecalho, sizeof(cabecalho), titulo);
+
+    thArgs.isFinished = SCR_PROCESSANDO;
+    thArgs.cb_scrload = cbScrLoad;
+
+    ret = pthread_create(&thread, NULL, threadLoad, &thArgs);
+
+    do{
+
+        if(strlen(buffer_cabecalho) <  tam_cabecalho){
+            for(i = 0; i < tam_cabecalho; i++){
+                buffer_cabecalho[i] = cabecalho[atual_cabecalho];
+                if(atual_cabecalho < strlen(cabecalho)){
+                    atual_cabecalho++;
+                }
+            }
+        }
+        
+        atual_cabecalho = atual_cabecalho - (tam_cabecalho - 1);
+
+        for(i = 0; i < tam_cabecalho; i++){
+            buffer_cabecalho[i] = cabecalho[atual_cabecalho];
+            if(atual_cabecalho < strlen(cabecalho)){
+                atual_cabecalho++;
+            }else{
+                atual_cabecalho = 0;
+            }
+        }
+
+        memset(buffer_barra, 0, sizeof(buffer_barra));
+        for(i = 0; i < tam_barra; i++){
+
+            if(buffer_barra != NULL){
+                snprintf(auxiliar, sizeof(auxiliar), "%s", buffer_barra);
+            }else{
+                auxiliar == NULL;
+            }
+
+            if(i < (float) thArgs.porcentagem / 100 * (float) tam_barra){
+                snprintf(buffer_barra, sizeof(buffer_barra), "%s%c", auxiliar, RECT_CMP_BLK);
+            }else{
+                snprintf(buffer_barra, sizeof(buffer_barra), "%s%c", auxiliar, RECT_EMP_BLK);
+            }
+        }
+
+        memset(txtLoading, 0, sizeof(txtLoading));
+        snprintf(txtLoading, sizeof(txtLoading), "%s |%s| %d%%", buffer_cabecalho, buffer_barra, thArgs.porcentagem);
+        printf("\r%s", txtLoading);
+        Sleep(300);
+    }while(thArgs.isFinished == SCR_PROCESSANDO);
+
+    pthread_join(thread, NULL);
+
+    return SCR_SUCESSO;
+}
---
+++ libTxtScreen.h
@@ -0,0 +1,99 @@
+#ifndef INTERFACE_H
+#define INTERFACE_H
+
+/* CORES PARA O TERMINAL */
+#define  ALL_NORMAL "\033[0m"
+#define  FG_BLACK   "\033[22;30m"
+#define  FG_MAROON  "\033[22;31m"     /* Dark red */
+#define  FG_GREEN   "\033[22;32m"
+#define  FG_OLIVE   "\033[22;33m"     /* Brown */
+#define  FG_NAVY    "\033[22;34m"     /* Dark blue */
+#define  FG_PURPLE  "\033[22;35m"
+#define  FG_TEAL    "\033[22;36m"     /* Dark cyan */
+#define  FG_SILVER  "\033[22;37m"     /* Light gray */
+#define  FG_GRAY    "\033[1;30m"      /* Dark gray */
+#define  FG_RED     "\033[1;31m"      /* Light red */
+#define  FG_LIME    "\033[1;32m"      /* Light green */
+#define  FG_YELLOW  "\033[1;33m"
+#define  FG_BLUE    "\033[1;34m"
+#define  FG_FUCHSIA "\033[1;35m"      /* Light purple */
+#define  FG_AQUA    "\033[1;36m"      /* Light cyan */
+#define  FG_WHITE   "\033[1;37m"
+#define  BG_BLACK   "\033[40m"
+#define  BG_MAROON  "\033[41m"
+#define  BG_GREEN   "\033[42m"
+#define  BG_OLIVE   "\033[43m"
+#define  BG_NAVY    "\033[44m"
+#define  BG_PURPLE  "\033[45m"
+#define  BG_TEAL    "\033[46m"
+#define  BG_SILVER  "\033[47m"
+
+/* SIMBOLOS ESPECIAIS */
+#define RECT_SUP_DIR 187        // Canto superior direito
+#define RECT_SUP_ESQ 201        // Canto superior esquerdo
+#define RECT_INF_DIR 188        // Canto inferior direito
+#define RECT_INF_ESQ 200        // Canto inferior esquerdo
+#define RECT_BAR_HOR 205        // 
+#define RECT_BAR_VER 186
+#define DUAL_ARROW_D 175
+#define DUAL_ARROW_E 175
+#define RECT_CMP_BLK 219
+#define RECT_EMP_BLK 176
+
+/* TECLAS */
+#define KEY_UP      72
+#define KEY_DOWN    80
+#define KEY_RIGHT   77
+#define KEY_LEFT    75
+
+/* RETONOR */
+#define SCR_SUCESSO 1
+#define SCR_FAIL -1
+#define SCR_PROCESSANDO -2
+
+typedef struct threadLoadArgs TH_LOAD_ARGS;
+typedef int (*CB_SCRLOAD) (TH_LOAD_ARGS *param);
+
+typedef struct threadLoadArgs{
+    int porcentagem;
+    int isFinished;
+    CB_SCRLOAD cb_scrload;
+} TH_LOAD_ARGS;
+
+/*
+* @brief Defini a cor do terminal
+*/
+void scrClear();
+
+/*
+* @brief Reseta para a cor padrao
+*/
+void scrRstCor();
+
+/*
+* @brief Defini a cor do terminal
+* @param char *cor - Texto a ser enviado
+*/
+void scrSetCor(char *cor);
+
+/*
+* @brief Printa texto com cor
+* @param char *fgColor Constante da cor do texto
+* @param char *bgColor constante da cor do fundo
+* @param char *format - Texto a ser enviado
+* @param ... - Argumentos
+*/
+void scrPrint(char *fgColor, char *bgColor, char *format, ...);
+
+/*
+* @brief Printa texto com cor e pula linha
+* @param char *fgColor Constante da cor do texto
+* @param char *bgColor constante da cor do fundo
+* @param char *format - Texto a ser enviado
+* @param ... - Argumentos
+*/
+void scrPrintLn(char *fgColor, char *bgColor, char *format, ...);
+
+int scrLoad(char *titulo, CB_SCRLOAD cbScrLoad);
+
+#endif //MAP_H
